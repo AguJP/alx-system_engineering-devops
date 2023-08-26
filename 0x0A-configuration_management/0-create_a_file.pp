64eda45605176b7ac8283d16a8d7e0adc5fffc81
AguJP <agujpa@gmail.com>
2023-08-25 21:27:04 -0700

    Create a file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# Create a file in /tmp using Puppet. File path, permission, owner, froup and content included
+
+file { '/tmp/school':
+    content => 'I love Puppet',
+    mode => '0744',
+    owner => 'www-data',
+    group => 'www-data',
+}
